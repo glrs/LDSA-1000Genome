e3ee901846783ccd56278f331acbbb7e41903e2e
Tasos <glaros.t@gmail.com>
2015-05-27 01:19:39 +0200

    Update popManager.sh

--- popManager.sh
+++ popManager.sh
@@ -4,21 +4,21 @@ set -e
 for pop in {CLM,PUR,ACB,FIN,CHS,CDX,IBS,PEL,PJL,KHV,CHB,JPT,CEU,TSI,GBR,YRI,LWK,GWD,MSL,ESN,ASW,MXL,BEB,STU,ITU,GIH}
 do
 echo "--------------- START for $pop ----------------"
-hadoop dfs -mkdir /$pop
+hadoop dfs -mkdir path/$pop
 echo "Dir made for $pop"
-hadoop dfs -put /mnt/gen-vol/GenomeData/*$pop* /$pop/
+hadoop dfs -put /path/GenomeData/*$pop* path/$pop/
 echo "$pop population transferred into HDFS."
-rfile="$(hadoop dfs -ls /$pop | head -n 2 | xargs -0 -n 1 basename)"
-hadoop jar ../target/hadoop-bam-7.0.0-jar-with-dependencies.jar view -H /$pop/$rfile > headers/$pop.txt
+rfile="$(hadoop dfs -ls path/$pop | head -n 2 | xargs -0 -n 1 basename)"
+hadoop jar path/hadoop-bam-7.0.0-jar-with-dependencies.jar view -H path/$pop/$rfile > headers/$pop.txt
 echo "BAM Header for $pop taken."
-hadoop jar target/*-jar-with-dependencies.jar org.seqdoop.hadoop_bam.examples.TestBAM "file:/home/ubuntu/hadoop-bam-7.0.0/examples/headers/$pop.txt" /$pop/*.bam /GenomeResults/"$pop"_out
+hadoop jar path/*-jar-with-dependencies.jar org.seqdoop.hadoop_bam.examples.TestBAM "file:/path/headers/$pop.txt" path/$pop/*.bam path/GenomeResults/"$pop"_out
 echo "END of HadoopBAM processing for $pop population."
-hadoop dfs -copyToLocal /GenomeResults/"$pop"_out/part-r-00000 /home/ubuntu/Results/"$pop"_Results
+hadoop dfs -copyToLocal path/GenomeResults/"$pop"_out/part-r-00000 /path/Results/"$pop"_Results
 echo "Results for $pop transferred out of HDFS."
-hadoop dfs -rmr /$pop
+hadoop dfs -rmr path/$pop
 echo "Folder $pop removed from HDFS."
 
-samtools view ../../Results/"$pop"_Results | ./bam_res_mapper.py | sort | ./reducer.py > ../../Results/"$pop"_counts_pp.txt
+samtools view path/Results/"$pop"_Results | ./bam_res_mapper.py | sort | ./reducer.py > path/Results/"$pop"_counts_pp.txt
 echo "Results analysis step for $pop finished."
 echo "--------------- END of $pop ----------------"
 
